e11faf86240928c2d353248c069c35f3a4750cc9
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-10 16:18:38 +0530

    add required.

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+    return (
+        <section>
+            <h1>You are logged in!</h1>
+            <br />
+            <p>
+                <a href="#">Go to Home</a>
+            </p>
+            <h1>Sign In</h1>
+            <form>
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" required />
+
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" required />
+
+                <button type="submit">Sign In</button>
+            </form>
+        </section>
+        
+    )
 }
 
 export default Login

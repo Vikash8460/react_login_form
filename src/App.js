848298d2b5fd83d6b6de24c7f7a3e5bb17e00c4f
Vikash8460 <Vikashkamat8080@gmail.com>
2024-04-11 10:53:35 +0530

    Add Register,js in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from './Login';
+import Register from './Register';
 function App() {
   return (
     <main className="App">
       <Login />
+    <Register/>
     </main>
   );
 }
